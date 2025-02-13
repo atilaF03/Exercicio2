b31c8fe7f2a94997a80c1ed5d72b93810bec751d
Átila <144128328+atilaF03@users.noreply.github.com>
2025-02-13 21:26:43 +0000

    atualizar

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App{
+public static void main(String[] args) {
+    for (int i = 1; i <=51; i+=2) {
+    System.out.println(i);
+    }
+    for (int i = 52; i <=100; i+=2) {
+        System.out.println(i);
+
+}
+}
+}
---
+++ App2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        System.out.println("Informe o numero ");
+        num = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+
+           
+            System.out.println("" + num + " X " + i + "=" + (num * i));
+        }
+        sc.close();
+    }
+
+}
